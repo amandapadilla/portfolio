70bfdff3a28b0cd97f52a7c048fb31f036a228da
amandapadilla <amandapadillasantiago@gmail.com>
2019-11-08 13:27:18 +0000

    build docs folder

---
+++ docs/precache-manifest.a38ffccdc9b2825ffcfcdea01dc11ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816c90990393849b88dabfd684cf5ddb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "404787c8424302d7593d",
+    "url": "./static/css/main.5d0a691a.chunk.css"
+  },
+  {
+    "revision": "12d489a24f8e2b653371",
+    "url": "./static/js/2.510e7dc3.chunk.js"
+  },
+  {
+    "revision": "404787c8424302d7593d",
+    "url": "./static/js/main.911a0936.chunk.js"
+  },
+  {
+    "revision": "d9fec5f6efaf04749bab",
+    "url": "./static/js/runtime-main.c6822cd3.js"
+  },
+  {
+    "revision": "fd5a13d8e676705f8aab48172e92bd53",
+    "url": "./static/media/background.fd5a13d8.png"
+  }
+]);
